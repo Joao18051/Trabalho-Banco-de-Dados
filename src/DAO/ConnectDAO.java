d36f173cf71e215e404ecfab57d503063f3e2694
Joao18051 <joao.magalhaes.s1805@gmail.com>
2023-12-12 16:37:19 -0300

    v0.1.1

--- src/DAO/ConnectDAO.java
+++ src/DAO/ConnectDAO.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public abstract class ConnectDAO{
     private static final String URL = "jdbc:postgresql://localhost:5432/oftamo_db";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Adriana35";
+    private static final String PASSWORD = "senha";
 
     protected Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
